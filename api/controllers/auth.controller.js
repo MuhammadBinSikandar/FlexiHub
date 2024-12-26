65e7957d0edce7276c8743be44ec099e2f259f8a
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2024-12-26 16:03:24 +0500

    Setting up the error handling, jwt tokenizaton

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,60 +1,60 @@
-import User from '../models/user.models.js';
+import User from '../models/user.model.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import createError from '../utils/createError.js';
 
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
   try {
     const hashedPassword = bcrypt.hashSync(req.body.password, 5);
-    console.log(hashedPassword)
-
     const newUser = new User({
       ...req.body,
       password: hashedPassword,
     });
+
     await newUser.save();
     res.status(201).send("User registered successfully");
   } catch (err) {
-    res.status(500).send("Server error");
+    next(err)
   }
 };
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if (!user) return res.status(404)
-      .send("User not found")
+    if (!user) return next(createError(404, "User not Found"));
 
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
 
-    if (!isCorrect) return res.status(400)
-      .send("Wrong password or username")
+    if (!isCorrect) return next(createError(400, "Wrong password or username"));
 
     const token = jwt.sign({
-      id:user._id,
-      isSeller:user.isSeller,
+      id: user._id,
+      isSeller: user.isSeller,
     },
-    process.env.JWT_KEY
-  );
-    
+      process.env.JWT_KEY
+    );
 
-    const {password, ...info}=user._doc
+    const { password, ...info } = user._doc
     res
-    .cookie("accessToken",token,{
-      httpOnly:true,
-    })
-    .status(200)
-    .send(info)
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info)
   } catch (err) {
-    res.status(500).send("Server error");
+    next(err);
   }
 };
 
 const logout = async (req, res) => {
-  // TODO: Implement logout functionality
-  res.status(200).send("Logout endpoint not implemented yet");
+  res.clearCookie("accessToken", {
+    sameSite: "None",
+    secure: true,
+  })
+  .status(200)
+  .send("Logged out");
 };
 
-// Export all functions
 export { register, login, logout };
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,13 @@
-export const deleteUser = (req, res) => {
-    res.send("delete user controller")
+import User from '../models/user.model.js'
+import createError from '../utils/createError.js'
+
+export const deleteUser = async (req, res) => {
+    const user = await User.findById(req.params.id)
+
+    if(req.userId !== user._id.toString()) {
+        return next(createError(403,"You are not authorized to delete this user"))
+    }
+
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).send("User has been deleted")
 }
