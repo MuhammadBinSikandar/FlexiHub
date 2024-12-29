d80c150417c0a06970d13da2b125079285c430ff
Muhammad Bin Sikandar <m.bin.sikandar@gmail.com>
2024-12-29 11:51:36 +0500

    Gigs backend setup queries

--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { deleteUser } from '../controllers/user.controller.js';
+import{
+    createGig,
+    deleteGig,
+    getGig,
+    getGigs,
+} from "../controllers/gig.controller.js";
+import { verifyToken } from '../middleware/jwt.js';
+
 
 const router = express.Router();
 
-router.get("/test", deleteUser)
+router.post("/", verifyToken, createGig);
+router.delete("/:id", verifyToken, deleteGig);
+router.get("/single/:id", verifyToken,getGig);
+router.get("/", verifyToken, getGigs);
 
 export default router;
